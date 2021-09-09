4ae55fa789e440c924325d9b549ad748b71547c0
Sebastian-jv <sebastian.tkaczykk@gmail.com>
2021-09-09 12:44:28 +0200

    v 1.0 add class App

---
+++ src/com/app/App.java
@@ -0,0 +1,25 @@
+package com.app;
+
+
+/*
+    Przygotuj klasę User. Klasa zawiera w sobie pola opisujące użytkownika, hasło oraz rolę (administrator lub user).
+    Dla klasy należy przewidzieć metody zapewniające jej poprawne działanie. Przygotuj kolekcję bez powtarzających się
+    elementów, która przechowuje przykładowe obiekty klasy User.
+
+    Następnie przygotuj klasę Dane, która zawiera w sobie mapę. Kluczem mapy jest napis – nazwa pliku tekstowego,
+    a wartością napis zawierający zawartość tego pliku. Dla klasy należy przewidzieć metody zapewniające jej poprawne
+    działanie. Mapa wypełniana jest za pomocą konstruktora argumentowego, który dostaje jako argument kolekcję z
+    niepowtarzającymi się nazwami plików tekstowych, z których należy czytać dane.
+
+    Klasa Przetwarzanie Danych zawiera w sobie mapę, której kluczem jest obiekt klasy User, natomiast wartością obiekt
+    klasy Dane. W klasie należy przewidzieć metodę dodającą do mapy kolejną parę użytkownik - dane. Każda para oznacza
+    użytkownika, który postanowił przetwarzać dane plików tekstowych podanych jako wartość w postaci obiektu klasy Dane.
+    Kiedy użytkownik jest administratorem może zmieniać zawartość plików modyfikując wartości w mapie klasy. Użytkownik
+    o zwykłych uprawnieniach może odczytać, ile razy w plikach wystąpił podany przez niego wzorzec. Na koniec wszystkie
+    pliki, są nadpisywane zmodyfikowaną zawartością, o ile użytkownik był administratorem. Na podstawie przedstawionego
+    opisu przygotuj aplikację, która zasymuluje przykładowy scenariusz zarządzania plikami.
+ */
+public class App {
+
+
+}
